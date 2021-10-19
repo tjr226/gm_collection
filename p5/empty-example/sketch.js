cbd6aa386ec882ea321850a29933a26b54360557
tjr226 <tjr226@gmail.com>
2021-10-18 20:03:22 -0400

    moving sun around the x and y axes

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -7,8 +7,18 @@ function setup() {
   // turn off additional borders
   strokeWeight(0);
   
-  for (let i = -1; i < 0; i++) {
+  var xOffsetArray = [0, 1, 2, 17];
+  var xOffsetArrayLength = xOffsetArray.length;
+
+  for (let i = 0; i < 8; i++) {
   //   sky
+
+    if (i == 3 || i == 4 || i == 5) {
+      continue
+      print(i)
+    }
+
+    for (let j = 0; j < xOffsetArrayLength; j++){
     
     fill('rgba(197, 202, 233, 1)');
     rect(0, 0, 24, 16);
@@ -29,8 +39,11 @@ function setup() {
   // (sun is made up of two overlaid rectangles - one 3x5, one 5x3)
     
     fill('rgba(240, 98, 145, 1)');
-    rect(18, 15 + i, 5, 3);
-    rect(19, 14 + i, 3, 5);
+    // rect(x start, y start, x length, y length)
+    // vert rect
+    rect(2 + xOffsetArray[j], 20 - i, 3, 5);
+    // horizontal rect
+    rect(1 + xOffsetArray[j], 21 - i, 5, 3);
     
   //   foreground
   //   (two different colors)
@@ -41,55 +54,15 @@ function setup() {
     fill('rgba(129, 199, 132, 1)');
     rect(0, 22, 24, 24);
 
-    saveCanvas(c, `myCanvas${i}`, 'jpg');
+    // save(c, `myCanvas`, 'jpg');
+    // save(`myCanvas`, 'jpg');
 
+    }
   }
 
+  
 }
 
 function draw() {
-  
-// //   (x start, y start, x length, y length)
-  
-//   strokeWeight(0);
-  
-// //   sky
-  
-//   fill('rgba(197, 202, 233, .7)');
-//   rect(0, 0, 24, 16);
-  
-// //   rayleigh
-  
-//   fill('rgba(255, 336, 179, 1)');
-//   rect(0, 16, 24, 16);
-     
-//   fill('rgba(255, 193, 7, 1)');
-//   rect(0, 17, 24, 19);
-  
-//   fill('rgba(245, 123, 0, 1)');
-//   rect(0, 20, 24, 20);
-  
-// // sun
-  
-//   fill('rgba(240, 98, 145, 1)');
-//   rect(18, 15, 5, 3);
-//   rect(19, 14, 3, 5);
-  
-// // // second sun
-// //   fill('rgba(255, 205, 210, 1)');
-// //   rect(21, 11, 2, 2);
-  
-// //   foreground
-  
-//   fill('rgba(165, 214, 167, 1)');
-//   rect(0, 21, 24, 21);
-  
-//   fill('rgba(129, 199, 132, 1)');
-//   rect(0, 22, 24, 24);
-  
-// //   moon
-  
-//   fill('rgba(252, 228, 236, 1)');
-//   rect(2, 2, 3, 3);
-  
+
 }
