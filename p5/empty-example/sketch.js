b91e872c46b092c6a110497dfb84903fd5da241c
tjr226 <tjr226@gmail.com>
2021-12-11 15:54:55 -0500

    commented out everything but the sun

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -15,25 +15,27 @@ function setup() {
 
     if (i == 3 || i == 4 || i == 5) {
       continue
-      print(i)
+      // print(i)
     }
 
     for (let j = 0; j < xOffsetArrayLength; j++){
     
-    fill('rgba(197, 202, 233, 1)');
-    rect(0, 0, 24, 16);
+
+      // the sky background, probably??
+    // fill('rgba(197, 202, 233, 1)');
+    // rect(0, 0, 24, 16);
     
   //   rayleigh
   //   (three different colors)
     
-    fill('rgba(255, 336, 179, 1)');
-    rect(0, 16, 24, 16);
+    // fill('rgba(255, 336, 179, 1)');
+    // rect(0, 16, 24, 16);
        
-    fill('rgba(255, 193, 7, 1)');
-    rect(0, 17, 24, 19);
+    // fill('rgba(255, 193, 7, 1)');
+    // rect(0, 17, 24, 19);
     
-    fill('rgba(245, 123, 0, 1)');
-    rect(0, 20, 24, 20);
+    // fill('rgba(245, 123, 0, 1)');
+    // rect(0, 20, 24, 20);
     
   // sun
   // (sun is made up of two overlaid rectangles - one 3x5, one 5x3)
@@ -48,11 +50,11 @@ function setup() {
   //   foreground
   //   (two different colors)
     
-    fill('rgba(165, 214, 167, 1)');
-    rect(0, 21, 24, 21);
+    // fill('rgba(165, 214, 167, 1)');
+    // rect(0, 21, 24, 21);
     
-    fill('rgba(129, 199, 132, 1)');
-    rect(0, 22, 24, 24);
+    // fill('rgba(129, 199, 132, 1)');
+    // rect(0, 22, 24, 24);
 
     // save(c, `myCanvas`, 'jpg');
     // save(`myCanvas`, 'jpg');
